315a39b7ab3974e7a662d103b94bf8b43148084c
sasha0552 <admin@sasha0552.org>
2023-06-28 06:22:33 +0600

    invoke "make hip" before packaging

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import os
 
 from setuptools import find_packages, setup
 
+os.system("make hip")
 libs = list(glob.glob("./bitsandbytes/libbitsandbytes*.so"))
 libs = [os.path.basename(p) for p in libs]
 print("libs:", libs)
